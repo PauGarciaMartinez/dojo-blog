1d5e24bebb699687d72e9b424b1704cbd545a012
paugarciamartinez <etpau@hotmail.es>
2021-05-06 23:58:25 +0200

    Establish fetch data structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,13 +17,22 @@ export default {
     PostList
   },
   setup() {
-    const posts = ref([
-      { title: 'Welcome to the blog', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id rutrum dolor, a volutpat nibh. Maecenas sit amet rutrum lorem. Ut eget est non neque blandit tristique. Vivamus aliquet, nunc non tempus elementum, nisl velit porta justo, in ullamcorper neque orci nec est. Phasellus accumsan bibendum finibus. Nullam maximus rhoncus placerat. Duis molestie a nisi vel vehicula. Nulla rhoncus blandit egestas. Sed et congue enim, porta porttitor nunc. Nunc auctor arcu id lectus accumsan, a accumsan ex interdum. Nulla tempus urna et nisi dictum imperdiet. Nam nec efficitur nulla, vitae consequat mi. Nulla volutpat fermentum tortor, et scelerisque justo.', id: 1 },
-      { title: 'Top 5 CSS tips', body: 'Maecenas gravida velit metus, a euismod massa rutrum non. Aenean sit amet arcu purus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce mollis tincidunt dolor, non pharetra magna luctus vitae. Curabitur vitae urna posuere tellus fermentum blandit in eu purus. Proin mattis nibh vitae urna tempor, a pharetra lectus aliquet. Integer eleifend semper mauris, ac molestie magna aliquam a. Donec non quam quis turpis consectetur euismod in at dolor. Nullam euismod, ipsum quis mollis tempus, purus ex lacinia metus, a commodo enim leo convallis diam.', id: 2 }
-    ]);
-    
-    const showPosts = ref(true);
+    const posts = ref([]);
+    const error = ref([]);
 
+    const load = async () => {
+      try {
+        let data = await fetch("http://localhost:3000/posts");
+        
+        if (!data.ok) {
+          throw new Error('No data available');
+        }
+
+      } catch(e) {
+        error.value = e.message;
+        console.log(eror.value);
+      }
+    }
 
     return { posts, showPosts }
   }
