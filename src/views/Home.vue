ca53cc4f2e6f781872a3c856574f3d1b4ebf2b8d
paugarciamartinez <etpau@hotmail.es>
2021-05-06 23:42:04 +0200

    Add some elements to the database json

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,7 +21,7 @@ export default {
       { title: 'Welcome to the blog', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id rutrum dolor, a volutpat nibh. Maecenas sit amet rutrum lorem. Ut eget est non neque blandit tristique. Vivamus aliquet, nunc non tempus elementum, nisl velit porta justo, in ullamcorper neque orci nec est. Phasellus accumsan bibendum finibus. Nullam maximus rhoncus placerat. Duis molestie a nisi vel vehicula. Nulla rhoncus blandit egestas. Sed et congue enim, porta porttitor nunc. Nunc auctor arcu id lectus accumsan, a accumsan ex interdum. Nulla tempus urna et nisi dictum imperdiet. Nam nec efficitur nulla, vitae consequat mi. Nulla volutpat fermentum tortor, et scelerisque justo.', id: 1 },
       { title: 'Top 5 CSS tips', body: 'Maecenas gravida velit metus, a euismod massa rutrum non. Aenean sit amet arcu purus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce mollis tincidunt dolor, non pharetra magna luctus vitae. Curabitur vitae urna posuere tellus fermentum blandit in eu purus. Proin mattis nibh vitae urna tempor, a pharetra lectus aliquet. Integer eleifend semper mauris, ac molestie magna aliquam a. Donec non quam quis turpis consectetur euismod in at dolor. Nullam euismod, ipsum quis mollis tempus, purus ex lacinia metus, a commodo enim leo convallis diam.', id: 2 }
     ]);
-
+    
     const showPosts = ref(true);
 
 
