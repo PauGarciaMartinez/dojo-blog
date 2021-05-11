0f79f47b327c3a6a82822313bb6cef2c2b8a66ad
Pau Garcia Martínez <etpau@hotmail.es>
2021-05-11 23:14:57 +0200

    Set form template and functionality

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -3,10 +3,18 @@
     <form>
       <label>Title</label>
       <input v-model="title" type="text" required>
+
       <label>Content</label>
       <textarea v-model="body" required></textarea>
+      
       <label>Tags (hit enter to add a tag)</label>
-      <input v-model="tag" type="text">
+      <input 
+        v-model="tag" 
+        type="text"
+        @keydown.enter.prevent="handleKeydown">
+
+      <div v-for="tag in tags" :key="tag" class="pill">#{{ tag }}</div>
+      
       <button>Add Post</button>
     </form>
   </div>
@@ -21,7 +29,15 @@ export default {
     const tag = ref('');
     const tags = ref([]);
 
-    return { title, body, tag, tags }
+    const handleKeydown = () => {
+      if (!tags.value.includes(tag.value)) {
+        tag.value = tag.value.replace(/\s/, '');
+        tags.value.push(tag.value);
+      }
+      tag.value = ''
+    }
+
+    return { title, body, tag, tags, handleKeydown }
   }
 }
 </script>
