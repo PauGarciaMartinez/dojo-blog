5cf9eec0987bebbbaafd02a064dcc2aa76d1a0fd
Pau Garcia Martínez <etpau@hotmail.es>
2021-05-11 23:06:45 +0200

    Create new view with a form to add posts

---
+++ src/views/Create.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="create">
+    <form>
+      <label>Title</label>
+      <input v-model="title" type="text" required>
+      <label>Content</label>
+      <textarea v-model="body" required></textarea>
+      <label>Tags (hit enter to add a tag)</label>
+      <input v-model="tag" type="text">
+      <button>Add Post</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+  setup() {
+    const title = ref('');
+    const body = ref('');
+    const tag = ref('');
+    const tags = ref([]);
+
+    return { title, body, tag, tags }
+  }
+}
+</script>
+
+<style>
+
+</style>
