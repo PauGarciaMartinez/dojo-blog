20d4f60851a099e76f6c186089b6adaddf20bef6
Pau Garcia Martínez <etpau@hotmail.es>
2021-06-02 16:37:44 +0200

    Set config.js file for firebase and firestore

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase/app'
+import 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyB0DuQ7lZkjN8jxOLXWdo9qPcNE0xsZ7Bs",
@@ -8,3 +9,11 @@ const firebaseConfig = {
   messagingSenderId: "287405955040",
   appId: "1:287405955040:web:84718421e18624842b3d85"
 };
+
+// Init Firebase
+firebase.initializeApp(firebaseConfig)
+
+// Init Firestore service
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
