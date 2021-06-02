23ba0617889b71ed234676f0c0b7d3d3f98a5496
Pau Garcia Martínez <etpau@hotmail.es>
2021-06-02 16:34:57 +0200

    Install and start firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB0DuQ7lZkjN8jxOLXWdo9qPcNE0xsZ7Bs",
+  authDomain: "my-dev-blog-67505.firebaseapp.com",
+  projectId: "my-dev-blog-67505",
+  storageBucket: "my-dev-blog-67505.appspot.com",
+  messagingSenderId: "287405955040",
+  appId: "1:287405955040:web:84718421e18624842b3d85"
+};
